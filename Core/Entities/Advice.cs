c68410df6c037024d8395a73d4139186853c33dc
Thiago Rozner <thiagorozner@gmail.com>
2023-09-08 23:29:56 -0300

    .

--- Core/Entities/Advice.cs
+++ Core/Entities/Advice.cs
@@ -4,26 +4,11 @@ namespace Core.Entities
 {
     public class Advice
     {
-        public Advice(int id, string model, string brand, int year, string color,
-            VehicleTransmissionTypeEnum transmission, string hodometer, int idUser, string description)
-        {
-            Id = id;
-            Model = model;
-            Brand = brand;
-            Year = year;
-            Color = color;
-            Transmission = transmission;
-            Hodometer = hodometer;
-            IdUser = idUser;
-            CreatedAt = DateTime.Now;
-            Status = true;
-            Description = description;
-        }
-
         public int Id { get; private set; }
         public string Model { get; private set; }
         public string Brand { get; private set; }
-        public int Year { get; private set; }
+        public int ModelYear { get; private set; }
+        public int ModelProductionYear { get; private set; }
         public string Color { get; private set; }
         public VehicleTransmissionTypeEnum Transmission { get; private set; }
         public string Hodometer { get; private set; }
